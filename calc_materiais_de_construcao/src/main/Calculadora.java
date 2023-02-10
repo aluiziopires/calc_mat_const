d01b3a9177304677a730c4dc8284994e9a3dc8af
aluiziopires <aluizioaf.pires@gmail.com>
2023-02-09 22:44:54 -0300

    Dia 2 Projeto 2

--- calc_materiais_de_construcao/src/main/Calculadora.java
+++ calc_materiais_de_construcao/src/main/Calculadora.java
@@ -11,33 +11,86 @@ public class Calculadora {
 		Scanner sc = new Scanner (System.in);
 		
 		int etapa;
+		double quantExec;
 		
 		System.out.println("Calculadora de materiais de construção:");
 		System.out.println(" ");
+		
+		do {
+		
 		System.out.println("Selecione a etapa atual da sua obra:");
 		System.out.println(" ");
 		System.out.println("1 - Alvenaria");
-		System.out.println("2 - Constrapiso");
+		System.out.println("2 - Contrapiso");
 		System.out.println("3 - Reboco");
 		System.out.println("4 - Cerâmica");
 		System.out.println("5 - Pintura");
+		System.out.println("6 - Sair");
 		System.out.println(" ");
 		System.out.print("Digite a etapa desejada: ");
 		etapa=sc.nextInt();
 		System.out.println(" ");
 		if (etapa==1) {
+			System.out.print("Quantidade a ser executada (m²): ");
+			quantExec=sc.nextDouble();
 			System.out.println("Materiais necessários para execução:");	
 			System.out.println(" ");
-			System.out.println("1 - Tijolo");	
+			System.out.println("1 - Tijolo");
 			System.out.println("2 - Massa de cimento e areia (traço 1:4)");
+			System.out.println(" ");
 				}
 		else {
-			System.out.println("0");	
-}	
+			if (etapa==2) {
+				System.out.print("Quantidade a ser executada (m²): ");
+				quantExec=sc.nextDouble();
+				System.out.println("Materiais necessários para execução:");	
+				System.out.println(" ");
+				System.out.println("1 - Massa de cimento e areia (traço 1:4)");	
+				System.out.println(" ");
+					}	
+			else {
+				if (etapa==3) {
+					System.out.print("Quantidade a ser executada (m²): ");
+					quantExec=sc.nextDouble();
+					System.out.println("Materiais necessários para execução:");	
+					System.out.println(" ");
+					System.out.println("1 - Massa de cimento e areia (traço 1:4)");	
+					System.out.println(" ");
+						}
+				else {
+					if (etapa==4) {
+						System.out.print("Quantidade a ser executada (m²): ");
+						quantExec=sc.nextDouble();
+						System.out.println("Materiais necessários para execução:");	
+						System.out.println(" ");
+						System.out.println("1 - Cerâmica");	
+						System.out.println("2 - Argamassa colante");
+						System.out.println("3 - Espaçador plástico");
+						System.out.println("4 - Rejunte");
+						System.out.println(" ");
+							}
+					else {
+						if (etapa==5) {
+							System.out.print("Quantidade a ser executada (m²): ");
+							quantExec=sc.nextDouble();
+							System.out.println("Materiais necessários para execução:");	
+							System.out.println(" ");
+							System.out.println("1 - Selador");	
+							System.out.println("2 - Massa corrida");
+							System.out.println("3 - Tinta");
+							System.out.println(" ");
+								}
+						else {
+							System.out.print("Programa finalizado!");	
+						}
+					}
+				}
+			}
+		}
 		
 		
 		
-		
+		}while (etapa<6);
 		
 		sc.close();	
 }
